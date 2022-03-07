fcb019b117974c037db8ba58262111cd473a6371
Domas <domasx2@gmail.com>
2022-03-07 17:06:49 +0200

    perf improvements

--- src/datasource.ts
+++ src/datasource.ts
@@ -24,7 +24,7 @@ const WIDTH_PX = 320;
 const HEIGHT_PX = 200;
 
 
-type RGB = [0, 0, 0];
+type RGB = [number, number, number];
 
 function rgbaKey(rgba: RGB): number {
   return rgba[0] * 1000 * 2 + rgba[1] * 1000 + rgba[2]
@@ -81,11 +81,14 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
 
       const colorsUsed: Record<number, boolean> = {}
 
+      const imgData = ctx.getImageData(0, 0, WIDTH_PX, HEIGHT_PX).data
+
       for (let x = 0; x < WIDTH_PX; x++) {
         let tLen = 1;
         const color2H: number[][] = Array(256);
         for (let y = 0; y < HEIGHT_PX; y++) {
-          const colorIdx = this.getColorIndex(ctx.getImageData(x, vflip ? y : HEIGHT_PX - y - 1, 1, 1).data as any as RGB)
+          const offst = ((vflip ? y : (HEIGHT_PX - y -1)) * WIDTH_PX + x) * 4;
+          const colorIdx = this.getColorIndex([imgData[offst], imgData[offst + 1], imgData[offst + 2]])
           colorsUsed[colorIdx] = true
           if (!color2H[colorIdx]) {
             color2H[colorIdx] = [y]
@@ -176,7 +179,13 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
         }
         this.getImg2DContext('/public/plugins/grafana-doom-datasource/img/title.png').then(ctx => {
           if (ctx) {
-            this.renderCanvas(ctx)
+            //this.renderCanvas(ctx)
+            
+            let flip = false;
+            this.intervalId = setInterval(() => {
+              flip = !flip;
+              this.renderCanvas(ctx, flip)
+            }, 1000 /25)
           }
         })
   
