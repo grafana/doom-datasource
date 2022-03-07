4e27f0e62a1eb18cff5e3286fdefa7e30d6d7205
Domas <domasx2@gmail.com>
2022-03-07 16:51:02 +0200

    first commit

---
+++ src/ConfigEditor.tsx
@@ -0,0 +1,50 @@
+import React, { ChangeEvent, PureComponent } from 'react';
+import { DataSourcePluginOptionsEditorProps } from '@grafana/data';
+import { MyDataSourceOptions } from './types';
+
+interface Props extends DataSourcePluginOptionsEditorProps<MyDataSourceOptions> {}
+
+interface State {}
+
+export class ConfigEditor extends PureComponent<Props, State> {
+  onPathChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { onOptionsChange, options } = this.props;
+    const jsonData = {
+      ...options.jsonData,
+      path: event.target.value,
+    };
+    onOptionsChange({ ...options, jsonData });
+  };
+
+  // Secure field (only sent to the backend)
+  onAPIKeyChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { onOptionsChange, options } = this.props;
+    onOptionsChange({
+      ...options,
+      secureJsonData: {
+        apiKey: event.target.value,
+      },
+    });
+  };
+
+  onResetAPIKey = () => {
+    const { onOptionsChange, options } = this.props;
+    onOptionsChange({
+      ...options,
+      secureJsonFields: {
+        ...options.secureJsonFields,
+        apiKey: false,
+      },
+      secureJsonData: {
+        ...options.secureJsonData,
+        apiKey: '',
+      },
+    });
+  };
+
+  render() {
+    return (
+      <div className="gf-form-group">DOOM!</div>
+    );
+  }
+}
---
+++ src/datasource.ts
@@ -0,0 +1,200 @@
+import defaults from 'lodash/defaults';
+
+import {
+  DataQueryRequest,
+  DataQueryResponse,
+  DataSourceApi,
+  DataSourceInstanceSettings,
+  dateTime,
+  FieldType,
+  LoadingState,
+  toDataFrame
+} from '@grafana/data';
+
+import { palette } from './palette';
+
+import { Observable, merge, Subscriber } from 'rxjs';
+
+import { MyQuery, MyDataSourceOptions, defaultQuery } from './types';
+
+//const FPS = 35;
+const RGBA_VALUE_ERROR_MARGIN = 3;
+
+const WIDTH_PX = 320;
+const HEIGHT_PX = 200;
+
+
+type RGB = [0, 0, 0];
+
+function rgbaKey(rgba: RGB): number {
+  return rgba[0] * 1000 * 2 + rgba[1] * 1000 + rgba[2]
+}
+
+interface RenderContext {
+  subscriber: Subscriber<DataQueryResponse>
+  query: MyQuery
+  options: DataQueryRequest<MyQuery>
+  twindow: number
+  rangeStep: number
+}
+
+export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
+
+  renderContext: RenderContext | null = null;
+
+  colorCache: Record<number, number> = {}
+
+  intervalId: any
+
+  constructor(instanceSettings: DataSourceInstanceSettings<MyDataSourceOptions>) {
+    super(instanceSettings);
+  }
+  
+  getColorIndex(rgb: RGB) {
+    const key = rgbaKey(rgb);
+    if (this.colorCache[key]) {
+      return this.colorCache[key];
+    }
+
+    for (let i =0; i < palette.length; i++) {
+      if ((Math.abs(palette[i][0] - rgb[0]) <= RGBA_VALUE_ERROR_MARGIN) &&
+          (Math.abs(palette[i][1] - rgb[1]) <= RGBA_VALUE_ERROR_MARGIN) &&
+          (Math.abs(palette[i][2] - rgb[2]) <= RGBA_VALUE_ERROR_MARGIN)
+          ){ 
+            this.colorCache[key] = i;
+            return i;
+          }
+    }
+    return 0;
+  }
+
+  renderCanvas(ctx: CanvasRenderingContext2D, vflip = false) {
+    if (this.renderContext) {
+      const renderContext = this.renderContext;
+      const now = dateTime().valueOf()
+      const start = now - this.renderContext.twindow;
+
+      const columns: Array<{
+        tLen: number,
+        color2H: number[][]
+      }> = []
+
+      const colorsUsed: Record<number, boolean> = {}
+
+      for (let x = 0; x < WIDTH_PX; x++) {
+        let tLen = 1;
+        const color2H: number[][] = Array(256);
+        for (let y = 0; y < HEIGHT_PX; y++) {
+          const colorIdx = this.getColorIndex(ctx.getImageData(x, vflip ? y : HEIGHT_PX - y - 1, 1, 1).data as any as RGB)
+          colorsUsed[colorIdx] = true
+          if (!color2H[colorIdx]) {
+            color2H[colorIdx] = [y]
+          } else {
+            color2H[colorIdx].push(y)
+            tLen = Math.max(tLen, color2H[colorIdx].length)
+          }
+        }
+        columns.push({ tLen, color2H })
+      }
+
+      const timeValues = columns.flatMap(({ tLen }, i) => Array.from(Array(tLen)).map((_, ti) => start + i * renderContext.rangeStep + ti))
+
+      const fields: any = [
+        { name: 'Time', type: FieldType.time, values: timeValues },
+      ]
+
+      Object.keys(colorsUsed).forEach((colorIndex) => {
+        const cIndex = Number(colorIndex)
+        const values: number[] = []
+        let i = 0;
+        for (let {tLen, color2H} of columns) {
+          if (color2H[cIndex]) {
+            color2H[cIndex].forEach((c, ii) => {
+              values[i + ii] = c
+            })
+          }
+          i+= tLen;
+        }
+
+        fields.push({ name: 'Value', type: FieldType.number, values, labels: { color: String(colorIndex) }})
+      })
+
+      const frame = toDataFrame({
+        name: 'screen',
+        fields,
+      });
+      frame.refId = renderContext.query.refId
+
+      renderContext.subscriber.next({
+        data: [frame],
+        key: renderContext.query.refId,
+        state: LoadingState.Streaming,
+      });
+    }
+  }
+
+
+  getImg2DContext(src: string): Promise<CanvasRenderingContext2D | null> {
+    return new Promise((resolve) => {
+      const canvas = document.createElement('canvas');
+      canvas.width = WIDTH_PX;
+      canvas.height = HEIGHT_PX;
+      const ctx = canvas.getContext('2d');
+      const img = new Image()
+      if (ctx) {
+        img.src = src;
+        img.onload = () => {
+          ctx.drawImage(img,0,0);
+          resolve(ctx)
+        }
+        img.onabort = () => resolve(null)
+        img.onerror = () => resolve(null)
+      } else {
+        resolve(null)
+      }
+    })
+  }
+
+  
+
+  query(options: DataQueryRequest<MyQuery>): Observable<DataQueryResponse> {
+    const streams = options.targets.map(target => {
+      const query = defaults(target, defaultQuery);
+
+      const from = options.range.from.valueOf();
+      const to = options.range.to.valueOf();
+      const diff = to- from;
+      const rangeStep = (to - from) / WIDTH_PX;
+
+      return new Observable<DataQueryResponse>(subscriber => {
+        this.renderContext = {
+          query: query,
+          options: options,
+          subscriber: subscriber,
+          twindow: diff,
+          rangeStep,
+        }
+        this.getImg2DContext('/public/plugins/grafana-doom-datasource/img/title.png').then(ctx => {
+          if (ctx) {
+            this.renderCanvas(ctx)
+          }
+        })
+  
+        return () => {
+          this.renderContext = null;
+          clearInterval(this.intervalId);
+        };
+      });
+    });
+  
+    return merge(...streams);
+  }
+
+  async testDatasource() {
+    // Implement a health check for your data source.
+    return {
+      status: 'success',
+      message: 'Success',
+    };
+  }
+}
